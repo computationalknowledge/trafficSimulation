ebfc4d7294b5e21491381d8a3cbce9a3313d100a
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2023-07-27 16:29:53 -0400

    Update startercode.java

--- startercode.java
+++ startercode.java
@@ -1,107 +1,94 @@
-
-write an object oriented java program to simulate an intersection: Model cars with speed and direction and accelerate and break methods , traffic lights, number of cars per minute driving north, south, east, west
-
-We'll create classes for cars and traffic lights and use them to simulate the intersection.
-
-First, let's define the Car class:
-
-public class Car {
-    private int speed;       // Speed in km/h
-    private String direction; // Direction: "N" (North), "S" (South), "E" (East), "W" (West)
-
-    public Car(int speed, String direction) {
-        this.speed = speed;
-        this.direction = direction;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void accelerate(int amount) {
-        speed += amount;
-    }
-
-    public void brake(int amount) {
-        speed = Math.max(0, speed - amount);
-    }
-
-    public String getDirection() {
-        return direction;
-    }
-}
-Next, let's define the TrafficLight class:
-
-public class TrafficLight {
-    private boolean isGreen;
-
-    public TrafficLight() {
-        isGreen = false; // Initialize traffic light to red
-    }
-
-    public boolean isGreen() {
-        return isGreen;
-    }
-
-    public void setGreen(boolean isGreen) {
-        this.isGreen = isGreen;
-    }
-}
-Now, we'll create the main class IntersectionSimulator to simulate the intersection and the flow of cars:
-
-java
-Copy code
-import java.util.Random;
-
-public class IntersectionSimulator {
-    private static final int SIMULATION_TIME_SECONDS = 60;
-
-    public static void main(String[] args) {
-        TrafficLight trafficLight = new TrafficLight();
-        Random rand = new Random();
-
-        // Create cars with random speeds and directions
-        Car[] cars = new Car[20];
-        for (int i = 0; i < cars.length; i++) {
-            int speed = rand.nextInt(60) + 20; // Speed between 20 and 80 km/h
-            String[] directions = {"N", "S", "E", "W"};
-            String direction = directions[rand.nextInt(directions.length)];
-            cars[i] = new Car(speed, direction);
+    
+    class Car {
+        private int speed;       // Speed in km/h
+        private String direction; // Direction: "N" (North), "S" (South), "E" (East), "W" (West)
+    
+        public Car(int speed, String direction) {
+            this.speed = speed;
+            this.direction = direction;
         }
-
-        // Simulate the intersection for a fixed number of seconds
-        for (int time = 0; time < SIMULATION_TIME_SECONDS; time++) {
-            System.out.println("Time: " + time + " seconds");
-
-            // Randomly set the traffic light to green for one direction
-            int randomDirectionIndex = rand.nextInt(4);
-            String[] directions = {"N", "S", "E", "W"};
-            String greenDirection = directions[randomDirectionIndex];
-            System.out.println("Traffic light is green for: " + greenDirection);
-            trafficLight.setGreen(true);
-
-            // Simulate car movements and update traffic light
-            for (Car car : cars) {
-                if (car.getDirection().equals(greenDirection)) {
-                    System.out.println("Car going " + car.getDirection() + " with speed " + car.getSpeed() + " km/h");
-                } else {
-                    System.out.println("Car waiting at red light: " + car.getDirection());
+    
+        public int getSpeed() {
+            return speed;
+        }
+    
+        public void accelerate(int amount) {
+            speed += amount;
+        }
+    
+        public void brake(int amount) {
+            speed = Math.max(0, speed - amount);
+        }
+    
+        public String getDirection() {
+            return direction;
+        }
+    }
+    
+    class TrafficLight {
+        private boolean isGreen;
+    
+        public TrafficLight() {
+            isGreen = false; // Initialize traffic light to red
+        }
+    
+        public boolean isGreen() {
+            return isGreen;
+        }
+    
+        public void setGreen(boolean isGreen) {
+            this.isGreen = isGreen;
+        }
+    }
+    
+    
+    
+    import java.util.Random;
+    
+    public class IntersectionSimulator {
+        private static final int SIMULATION_TIME_SECONDS = 60;
+    
+        public static void main(String[] args) {
+            TrafficLight trafficLight = new TrafficLight();
+            Random rand = new Random();
+    
+            // Create cars with random speeds and directions
+            Car[] cars = new Car[20];
+            for (int i = 0; i < cars.length; i++) {
+                int speed = rand.nextInt(60) + 20; // Speed between 20 and 80 km/h
+                String[] directions = {"N", "S", "E", "W"};
+                String direction = directions[rand.nextInt(directions.length)];
+                cars[i] = new Car(speed, direction);
+            }
+    
+            // Simulate the intersection for a fixed number of seconds
+            for (int time = 0; time < SIMULATION_TIME_SECONDS; time++) {
+                System.out.println("Time: " + time + " seconds");
+    
+                // Randomly set the traffic light to green for one direction
+                int randomDirectionIndex = rand.nextInt(4);
+                String[] directions = {"N", "S", "E", "W"};
+                String greenDirection = directions[randomDirectionIndex];
+                System.out.println("Traffic light is green for: " + greenDirection);
+                trafficLight.setGreen(true);
+    
+                // Simulate car movements and update traffic light
+                for (Car car : cars) {
+                    if (car.getDirection().equals(greenDirection)) {
+                        System.out.println("Car going " + car.getDirection() + " with speed " + car.getSpeed() + " km/h");
+                    } else {
+                        System.out.println("Car waiting at red light: " + car.getDirection());
+                    }
+                }
+    
+                trafficLight.setGreen(false); // Set the traffic light back to red
+                System.out.println("------------------");
+    
+                try {
+                    Thread.sleep(1000); // Wait for 1 second in the simulation
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-
-            trafficLight.setGreen(false); // Set the traffic light back to red
-            System.out.println("------------------");
-
-            try {
-                Thread.sleep(1000); // Wait for 1 second in the simulation
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
     }
-}
-This simulation will randomly set the traffic light to green for one direction at a time, allowing cars from that direction to move through the intersection. 
-  The simulation will print the car's direction, speed, and whether it's waiting at a red light during each second of the simulation.
-
-Keep in mind that this is a basic simulation and does not consider more complex traffic patterns, collision detection, or actual car movement in real time. 
-  You can further enhance the simulation to include more advanced features and realistic behavior as per your requirements.
